4612cf4aefdd3735665d24bee2a6d10b1fb6b202
Lukyan Zhukov <l.zhukov@city-mobil.ru>
2021-11-14 19:17:05 +0700

    Add sample test to android ci.

---
+++ app/src/test/java/ru/lukianbat/netologytest/SampleTest.kt
@@ -0,0 +1,12 @@
+package ru.lukianbat.netologytest
+
+import org.junit.Assert
+import org.junit.Test
+
+class SampleTest {
+    @Test
+    fun sampleTestMethod() {
+        val testNumber = 0
+        Assert.assertEquals(testNumber, 0)
+    }
+}
